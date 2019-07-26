787e50ed0708a0efe00f668735d4963c00010835
yun-chien <53063596+yun-chien@users.noreply.github.com>
2019-07-25 21:17:19 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "Hello~"
+export DEBIAN_FRONTEND='non-interactive'
+
+echo -e "slapd slapd/root_password password admin" |debconf-set-selections
+echo -e "slapd slapd/root_password_again password admin" |debconf-set-selections
+echo -e "slapd slapd/internal/adminpw password admin" |debconf-set-selections
+echo -e "slapd slapd/internal/generated_adminpw password admin" |debconf-set-selections
+echo -e "slapd slapd/password2 password admin" |debconf-set-selections
+echo -e "slapd slapd/password1 password admin" |debconf-set-selections
