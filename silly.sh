c3880809eec80757c40be0974feaa68e2aae7129
yun-chien <53063596+yun-chien@users.noreply.github.com>
2019-07-30 20:23:26 -0400

    Update silly.sh

--- silly.sh
+++ silly.sh
@@ -7,3 +7,8 @@ echo "This is a Apache installation script" > /tmp/silly.txt
 sudo apt-get update -y
 sudo apt-get install -y apache2
 sudo systemctl restart apache2
+sudo apt-get install -y slapd ldap-utils
+sudo dpkg-reconfigure slapd
+sudo ufw allow ldap
+ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -W -f basedn.ldif
+slappasswd
